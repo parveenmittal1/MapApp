02246e9f58d8846183a89dc65988494af88337cc
parveen <mittalparveen652@gmail.com>
2020-05-28 16:44:07 +0530

    build folder uploaded

---
+++ client/docs/precache-manifest.e1d8e7208a65c00850d5406cdacb9818.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3028e87d4f8e67b9d826648e4071024d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32a7a5de4459195bac53",
+    "url": "/static/css/2.665191fe.chunk.css"
+  },
+  {
+    "revision": "999e6fe9971953ee4d23",
+    "url": "/static/css/main.ec53640f.chunk.css"
+  },
+  {
+    "revision": "32a7a5de4459195bac53",
+    "url": "/static/js/2.67e4a22c.chunk.js"
+  },
+  {
+    "revision": "1f28fc3eee0a645225bdaffd4c69bdd7",
+    "url": "/static/js/2.67e4a22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999e6fe9971953ee4d23",
+    "url": "/static/js/main.4336e451.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "832362f3b72fc7ac2f69bc8e96bf7d0d",
+    "url": "/static/media/client.832362f3.svg"
+  },
+  {
+    "revision": "da7c58865dd3bb44748a03828b4525f7",
+    "url": "/static/media/mappin.da7c5886.svg"
+  }
+]);
